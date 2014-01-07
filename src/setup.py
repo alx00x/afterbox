9f1a3c01ff7f8eab491ddda57f7cacf02518f541
Axy Mark <axymark@gmail.com>
2014-01-07 02:53:24 +0100

    Added simple installer/updater

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe, sys, os
+
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    windows = [{"script":"eipixTools_install.py"}],
+    zipfile = None,
+    data_files=[(".",
+        ["eipixTools_install.ico"])],
+)
