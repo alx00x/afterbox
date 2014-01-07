def6bb9810c6df798e4e95022a833058d2fa56e5
Axy Mark <axymark@gmail.com>
2014-01-07 22:49:39 +0100

    Small updates to add scripts.

--- (eipixTools)/addElement.jsx
+++ (eipixTools)/addElement.jsx
@@ -11,11 +11,33 @@
 
 (function addElement(thisObj) {
     var activeItem = app.project.activeItem;
-    var solidName = "element";
+    var selectedLayer = activeItem.selectedLayers[0];
+    var layerNumber = 0;
+    var layerName;
+    var testNumber;
+
+    for (var i = 1; i <= app.project.numItems; i++) {
+        if ((app.project != null) && (app.project.item(i).mainSource instanceof SolidSource)) {
+            layerName = app.project.item(i).name;
+            if (layerName.indexOf("element ") != -1) {
+                testNumber = parseInt(layerName.substring(layerName.lastIndexOf(" "), layerName.length));
+                if (!isNaN(testNumber)) {
+                    layerNumber = Math.max(layerNumber, testNumber);
+                }
+            }
+        }
+    }
+
+    app.beginUndoGroup("addElement");
+
+    var solidName = "element " + (layerNumber + 1);
     var solidW = activeItem.width;
     var solidH = activeItem.height;
     var solidPixelAspectRatio = activeItem.pixelAspect;
     var solidDuration = activeItem.duration;
     var newSolid = activeItem.layers.addSolid([0, 0, 0], solidName, solidW, solidH, solidPixelAspectRatio, solidDuration);
+    newSolid.moveBefore(selectedLayer);
     var addParticular = newSolid.property("Effects").addProperty("Element");
+
+    app.endUndoGroup();
 })(this);
--- (eipixTools)/addParticular.jsx
+++ (eipixTools)/addParticular.jsx
@@ -11,11 +11,33 @@
 
 (function addParticular(thisObj) {
     var activeItem = app.project.activeItem;
-    var solidName = "particular";
+    var selectedLayer = activeItem.selectedLayers[0];
+    var layerNumber = 0;
+    var layerName;
+    var testNumber;
+
+    for (var i = 1; i <= app.project.numItems; i++) {
+        if ((app.project != null) && (app.project.item(i).mainSource instanceof SolidSource)) {
+            layerName = app.project.item(i).name;
+            if (layerName.indexOf("particular ") != -1) {
+                testNumber = parseInt(layerName.substring(layerName.lastIndexOf(" "), layerName.length));
+                if (!isNaN(testNumber)) {
+                    layerNumber = Math.max(layerNumber, testNumber);
+                }
+            }
+        }
+    }
+
+    app.beginUndoGroup("addParticular");
+
+    var solidName = "particular " + (layerNumber + 1);
     var solidW = activeItem.width;
     var solidH = activeItem.height;
     var solidPixelAspectRatio = activeItem.pixelAspect;
     var solidDuration = activeItem.duration;
     var newSolid = activeItem.layers.addSolid([0, 0, 0], solidName, solidW, solidH, solidPixelAspectRatio, solidDuration);
+    newSolid.moveBefore(selectedLayer);
     var addParticular = newSolid.property("Effects").addProperty("Particular");
+
+    app.endUndoGroup();
 })(this);
