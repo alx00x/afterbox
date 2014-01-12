6de3a7a1318aec6290b893893449f5f408cb12b5
Axy Mark <axymark@gmail.com>
2014-01-12 19:22:05 +0100

    Added audioTimecode script placeholder

---
+++ (eipixTools)/audioTimecode.jsx
@@ -0,0 +1,14 @@
+﻿// audioTimecode.jsx
+// 
+// Name: audioTimecode
+// Version: 1.0
+// Author: Aleksandar Kocic
+// 
+// Description:     
+// 
+//  
+
+
+(function audioTimecode(thisObj) {
+    // code
+})(this);
