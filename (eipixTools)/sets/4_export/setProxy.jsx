970cacc06776e2b4a3e4d621daa06c3d6cb5bae5
Axy Mark <axymark@gmail.com>
2014-05-03 18:36:58 +0200

    setProxy init

---
+++ (eipixTools)/sets/4_export/setProxy.jsx
@@ -0,0 +1,15 @@
+// setProxy.jsx
+// 
+// Name: setProxy
+// Version: 0.1
+// Author: Aleksandar Kocic
+// 
+// Description:     
+// Creates proxy png sequences for each selected element.
+//  
+
+
+(function setProxy(thisObj)
+{
+    //code
+})(this);
