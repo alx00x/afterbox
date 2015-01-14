3936faf80a3788b1abe850967c5e78e8c4fd184f
koaleksa <koaleksa@gmail.com>
2015-01-14 10:31:32 +0100

    onion skinning v1.0

--- files/(eipixTools)/sets/5_animation/onionSkinning.jsx
+++ files/(eipixTools)/sets/5_animation/onionSkinning.jsx
@@ -1,7 +1,7 @@
 ﻿// onionSkinning.jsx
 // 
 // Name: onionSkinning
-// Version: 0.2
+// Version: 1.0
 // Author: Aleksandar Kocic
 // 
 // Description:     
@@ -20,14 +20,11 @@
 
     onsData.scriptNameShort = "ONS";
     onsData.scriptName = "Onion Skinning";
-    onsData.scriptVersion = "0.2";
+    onsData.scriptVersion = "1.0";
     onsData.scriptTitle = onsData.scriptName + " v" + onsData.scriptVersion;
 
     onsData.strMinAE = {en: "This script requires Adobe After Effects CS6 or later."};
     onsData.strActiveCompErr = {en: "Please select a composition."};
-    onsData.strMargine = {en: "Margine"};
-    onsData.strBefore = {en: "Before"};
-    onsData.strAfter = {en: "After"};
 
     onsData.strHelp = {en: "?"};
     onsData.strHelpTitle = {en: "Help"};
@@ -44,11 +41,42 @@
     // Main Functions
     //
     function onionSkinning_main() {
+        //define
+        var OnionSkinning = "Onion Skinning";
+        var Margine = "Margine";
+        var Before = "Before";
+        var After = "After";
+
         //add adjustment layer
+        var active = onsData.activeItem;
+        var adjLayer = active.layers.addSolid([0,0,0], OnionSkinning, active.width, active.height, 1);
+        adjLayer.adjustmentLayer = true;
+
         //move to top
+        adjLayer.moveToBeginning();
+
         //add slider
+        var slider = adjLayer.property("ADBE Effect Parade").addProperty("ADBE Slider Control");
+        slider.name = Margine;
+        slider.property("ADBE Slider Control-0001").setValue(4);
+
         //add echo one
+        var echo1 = adjLayer.property("ADBE Effect Parade").addProperty("ADBE Echo");
+        var expr1 = "x = effect(\"" + Margine + "\")(\"Slider\");\n-(thisComp.frameDuration * x);";
+        echo1.name = Before;
+        echo1.property("ADBE Echo-0001").expressionEnabled = true;
+        echo1.property("ADBE Echo-0001").expression = expr1;
+        echo1.property("ADBE Echo-0004").setValue(0.5);
+        echo1.property("ADBE Echo-0005").setValue(2);
+
         //add echo two
+        var echo2 = adjLayer.property("ADBE Effect Parade").addProperty("ADBE Echo");
+        var expr2 = "x = effect(\"" + Margine + "\")(\"Slider\");\nthisComp.frameDuration * x;";
+        echo2.name = After;
+        echo2.property("ADBE Echo-0001").expressionEnabled = true;
+        echo2.property("ADBE Echo-0001").expression = expr2;
+        echo2.property("ADBE Echo-0004").setValue(0.5);
+        echo2.property("ADBE Echo-0005").setValue(2);
     }
 
     // Init
