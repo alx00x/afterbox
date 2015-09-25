77201741b19c8d5a6a41b0c9b33cdb17f3b88c5c
koaleksa <koaleksa@gmail.com>
2015-09-25 16:31:33 +0200

    fixHalfFrameKeys.jsx v 1.0

--- files/(eipixTools)/sets/5_animation/fixHalfFrameKeys.jsx
+++ files/(eipixTools)/sets/5_animation/fixHalfFrameKeys.jsx
@@ -25,6 +25,8 @@
 
     fhfkData.strMinAE = {en: "This script requires Adobe After Effects CS4 or later."};
     fhfkData.strActiveCompErr = {en: "Please select a composition."};
+    fhfkData.strSelectedLayersErr = {en: "No layer is selected."};
+    fhfkData.strSelectedPropertiesErr = {en: "No property is selected."};
     fhfkData.strSelectedKeysErr = {en: "Select at least one key to be fixed."};
 
     fhfkData.strHelp = {en: "?"};
@@ -33,25 +35,138 @@
 
     // Define project variables
     fhfkData.activeItem = app.project.activeItem;
+    fhfkData.activeItemFPS = app.project.activeItem.frameRate;
 
     // Localize
     function fixHalfFrameKeys_localize(strVar) {
         return strVar["en"];
     }
 
+    //fix keyframe time
+    function fixHalfFrameKeys_fixKeys(layerIndex, propertyPathArray, keyIndex, keyValue, nearestFrame) {
+        //form property path 
+        var propertyPathTemp = propertyPathArray;
+        var propertyPath = [];
+        for (var i = 0; i < propertyPathTemp.length; i++) {
+            propertyPath.push("(" + propertyPathTemp[i].toString() + ")");
+        }
+        propertyPath = propertyPath.reverse().join("");
+
+        //delete key
+        var deleteKeyCommand = "app.project.activeItem.layer(" + layerIndex.toString() + ")" + propertyPath.toString() + ".removeKey(" + keyIndex.toString() + ")";
+        eval(deleteKeyCommand);
+
+        //create key at nearest frame
+        var addKeyCommand = "app.project.activeItem.layer(" + layerIndex.toString() + ")" + propertyPath.toString() + ".addKey(" + nearestFrame.toString() + ")";
+        eval(addKeyCommand);
+
+        //set value
+        var setValueCommand = "app.project.activeItem.layer(" + layerIndex.toString() + ")" + propertyPath.toString() + ".setValueAtKey(" + keyIndex.toString() + ",[" + keyValue.toString() + "])";
+        eval(setValueCommand);
+    }
+
+    //buld property path
+    /*function fixHalfFrameKeys_buildPropPath(property) {
+        var path;
+        var propertyDepth = property.propertyDepth;
+
+        for 
+
+        return path;
+    }*/
+
     // Main Functions
     //
     function fixHalfFrameKeys_main() {
-        //code
+        //variables
+        var activeItem = fhfkData.activeItem;
+        var activeItemFPS = fhfkData.activeItemFPS;
+        var keyInfoArray = [];
 
-        //
+        //get selected layers
+        var selectedLayers = activeItem.selectedLayers;
 
-        //
-        
-        //
-        
-        //
-        
+        /*if (selectedLayers.length < 1) {
+            alert(fixHalfFrameKeys_localize(fhfkData.strSelectedLayersErr));
+            return;
+        }*/
+
+        //for all selected layers
+        for (var i = 0; i < selectedLayers.length; i++) {
+            //get selected layers index
+            var layerIndex = selectedLayers[i].index;
+
+            //get selected property
+            var selectedProperties = selectedLayers[i].selectedProperties;
+
+            /*if (selectedProperties.length < 1) {
+                alert(fixHalfFrameKeys_localize(fhfkData.strSelectedPropertiesErr));
+                return;
+            }*/
+
+            //for all selected properties that have animation
+            for (var a = 0; a < selectedProperties.length; a++) {
+                if (selectedProperties[a].numKeys > 0) {
+                    //get selected properties info
+                    var propertyDepth = selectedProperties[a].propertyDepth;
+
+                    //buld property path
+                    //var propertyPath = fixHalfFrameKeys_buildPropPath(selectedProperties[a]);
+                    var propertyPathArray = [];
+                    var currentProperty = selectedProperties[a];
+                    while (currentProperty.parentProperty != null) {
+                        propertyPathArray.push(currentProperty.propertyIndex);
+                        currentProperty = currentProperty.parentProperty;
+                    }
+    
+                    //get selected keys
+                    var selectedKeys = selectedProperties[a].selectedKeys;
+    
+                    /*if (selectedKeys.length < 1) {
+                        alert(fixHalfFrameKeys_localize(fhfkData.strSelectedKeysErr));
+                        return;
+                    }*/
+    
+                    //get number of keyframes
+                    var numberOfKeys = selectedProperties[a].numKeys;
+    
+                    //get selected keys inicies
+                    var selectedKeysIndicies = [];
+                    for (var d = 1; d <= numberOfKeys; d++) {
+                        if (selectedProperties[a].keySelected(d) == true) {
+                            selectedKeysIndicies.push(d);
+                        }
+                    }
+    
+                    //for all selected keys
+                    for (var f = 0; f < selectedKeysIndicies.length; f++) {
+                        //get key index
+                        var keyIndex = selectedKeysIndicies[f];
+    
+                        //get value
+                        var keyValue = selectedProperties[a].keyValue(keyIndex);
+    
+                        //get time
+                        var keyTime = selectedProperties[a].keyTime(keyIndex);
+    
+                        //nearest full frame
+                        var nearestFrame = Math.round(keyTime * activeItemFPS) / activeItemFPS; // round to nearest keyframe value;
+    
+                        //form this keys info array
+                        var thisKeysInfo = [layerIndex, propertyPathArray, keyIndex, keyValue, nearestFrame];
+    
+                        //push this keys info into main array
+                        keyInfoArray.push(thisKeysInfo);
+                    }
+                }
+            }
+        }
+
+        //fix keys
+        for (var d = 0; d < keyInfoArray.length; d++) {
+            var currentKey = keyInfoArray[d];
+            fixHalfFrameKeys_fixKeys(currentKey[0], currentKey[1], currentKey[2], currentKey[3], currentKey[4]);
+        }
     }
 
     // Init
